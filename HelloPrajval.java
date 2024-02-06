487cf63467add6abb30e3281de567efa8344483b
EC2 Default User <ec2-user@ip-172-31-37-142.ap-south-1.compute.internal>
2024-02-06 06:23:04 +0000

    added loan clear

--- HelloPrajval.java
+++ HelloPrajval.java
@@ -1,6 +1,8 @@
 public class HelloPrajval {
   public static void main(String[] args) {
     System.out.println("Hello Prajval is unlucky");
+  System.out.println("i want to clear my all loans");
     System.out.println("Hello Praj is very unlucky");
   }
+
 }
