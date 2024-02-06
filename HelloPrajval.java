d027ebbe5cd064a9903561fdee49a13c15dc305a
EC2 Default User <ec2-user@ip-172-31-37-142.ap-south-1.compute.internal>
2024-02-06 06:07:16 +0000

    added very unclucky

--- HelloPrajval.java
+++ HelloPrajval.java
@@ -1,5 +1,6 @@
 public class HelloPrajval {
   public static void main(String[] args) {
     System.out.println("Hello Prajval is unlucky");
+    System.out.println("Hello Praj is very unlucky");
   }
 }
